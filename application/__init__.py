3527e9efbb4648819e3350a1e0a6ab97aebbfdd6
Dreamer1007 <meriem.batata53@gmail.com>
2020-04-06 23:15:08 +0100

    put smartboard title

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ad2438f83788a4e4225f5ef0db61a266'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -13,6 +12,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+ 
 
 
 from application import routes
