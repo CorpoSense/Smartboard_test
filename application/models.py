b7b681d4bbb62ce365ae48cd9b4d61c8ee75917f
yas-mine <49839182+yas-mine@users.noreply.github.com>
2020-04-25 15:52:46 +0200

    delet sheet file

--- application/models.py
+++ application/models.py
@@ -7,7 +7,6 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
